cd024e8c8b90f569a7265bc59dc9aeff8184cc79
prakashvenigandla <venigandla.prakash@hotmail.com>
2021-04-26 14:41:22 +0530

    datatypes demo added

---
+++ dotnet/demos/DataTypesDemo/DataTypesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesDemo
+{
+    class Program
+    {
+        enum slabs{slab1=50,slab2=100};
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("enter choice");
+            choice=Convert.ToInt32(Console.ReadLine());
+           switch(choice)
+           {
+               case slabs.slab1
+        }
+    }
+}
